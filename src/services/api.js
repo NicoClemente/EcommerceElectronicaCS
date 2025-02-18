76d1b36978dfb13f0fadca8c00dc37a904ce7c81
nicoc <clementes.nicolas@gmail.com>
2025-02-18 19:47:23 -0300

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
  
 import axios from 'axios';
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
-const baseURL = ' https://apinodeecommerce.onrender.com/api/ '; 
+const baseURL = 'https://apinodeecommerce.onrender.com/api/'; 
 
 
 const api = axios.create({
